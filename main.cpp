aec02be2c23f1e391c3a46d59f81a7a23a57a54d
MuZi-lh <61682147+MuZi-lh@users.noreply.github.com>
2021-04-12 09:34:26 +0800

    1152 finished

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
 //
 // Created by 木子又欠 on 2021/4/12.
 //
+#include<iostream>
+#include<string>
+using namespace std;
+
+int K, L, lo;
+
+int isPrime(int k) {
+	for (int i = 2;i * i < k;i++) {
+		if (k % i == 0) return 0;
+	}
+	return 1;
+}
+
+int main() {
+	string s;
+	cin >> L >> K >> s;
+	for (int i = 0;i <= L - K;i++) {
+		string t = s.substr(i, K);
+		int num = stoi(t);
+		if (isPrime(num)) {
+			cout << t;
+			return 0;
+		}
+	}
+	cout << "404\n";
+}
