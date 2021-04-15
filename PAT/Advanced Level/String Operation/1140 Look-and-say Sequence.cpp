211b082b8ebb5f1575d7a76d7dcc2cef94cb971d
MuZi-lh <61682147+MuZi-lh@users.noreply.github.com>
2021-04-15 20:39:40 +0800

    1140 finished

---
+++ PAT/Advanced Level/String Operation/1140 Look-and-say Sequence.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Ä¾×ÓÓÖÇ· on 2021/4/15.
+//
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s;
+	int n, j;
+	cin >> s >> n;
+	for (int cnt = 1; cnt < n; cnt++) {
+		string t;
+		for (int i = 0; i < s.length(); i = j) {
+			for (j = i; j < s.length() && s[j] == s[i]; j++);
+			t += s[i] + to_string(j - i);
+		}
+		s = t;
+	}
+	cout << s;
+	return 0;
+}
